7a2180d51a4b45fa0996a4a78de21dff1e8c84ec
Murad <mrdaydinov@gmail.com>
2023-10-22 03:28:50 +0400

    Add ProductsController

---
+++ ProductsApi/ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Repository;
+using System.Runtime.CompilerServices;
+
+namespace ProductsApi.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsRepository _productsRepository;
+
+        public ProductsController(IProductsRepository productsRepository)
+        {
+               _productsRepository = productsRepository;
+        }
+    }
+}
