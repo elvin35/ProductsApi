acbb291764e46d4e27c237e50de532c8b2210eb2
Elvin Mammadov <elvin.mamedov.2013@gmail.com>
2023-10-22 01:10:17 +0400

    Add ProductsModel

---
+++ ProductsApi/ProductsApi/Models/ProductsModel.cs
@@ -0,0 +1,13 @@
+namespace ProductsApi.Models;
+
+public class ProductsModel
+{
+   
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    
+    public decimal Price { get; set; } 
+    
+    public string Description { get; set; }
+}
