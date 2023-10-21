73559cb1c10b2abaa16c780c868ae5fd65ed2ce6
Murad <mrdaydinov@gmail.com>
2023-10-22 02:16:48 +0400

    Add Add Method

---
+++ ProductsApi/ProductsApi/Repository/ProductsRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsApi.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Repository
+{
+    public class ProductsRepository : IProductsRepository
+    {
+        private readonly ProductsApiDbContext _context;
+
+        public ProductsRepository(ProductsApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Add(Products product)
+        {
+            var names = await _context.Products.Select(product => product.Name).ToListAsync();
+            foreach (var name in names)
+            {
+                if (product.Name == name)
+                {
+                    return false;
+                }
+            }
+            await _context.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public Task<List<Products>> GetAll()
+        {
+            
+        }
+
+        public Task<bool> Remove(string name)
+        {
+            
+        }
+
+        public Task<bool> Update(string name, Products product)
+        {
+            
+        }
+    }
+}
