3f1eee7774910bc159a8aeed335acd5ff6682c9d
Elvin Mammadov <elvin.mamedov.2013@gmail.com>
2023-10-22 02:15:56 +0400

    Add profile to automapper

---
+++ ProductsApi/ProductsApi/Profiles/ApplicationProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using ProductsApi.Data;
+using ProductsApi.Models;
+
+namespace ProductsApi.Profiles;
+
+public class ApplicationProfile : Profile
+{
+    public ApplicationProfile()
+    {
+        CreateMap<Products, UpdateProduct>().ReverseMap();
+        CreateMap<Products, ProductsModel>().ReverseMap();
+    }   
+}
