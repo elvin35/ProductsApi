393ca0a1a7f472de516229555c73090d3c0f7eb8
Murad <mrdaydinov@gmail.com>
2023-10-22 03:32:08 +0400

    Add GetAllProducts Method with mapper

--- ProductsApi/ProductsApi/Controllers/ProductsController.cs
+++ ProductsApi/ProductsApi/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Models;
 using ProductsApi.Repository;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace ProductsApi.Controllers
 {
@@ -9,10 +13,19 @@ namespace ProductsApi.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductsRepository _productsRepository;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductsRepository productsRepository)
+        public ProductsController(IProductsRepository productsRepository, IMapper mapper)
         {
-               _productsRepository = productsRepository;
+            _productsRepository = productsRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = _mapper.Map<List<ProductsModel>>(await _productsRepository.GetAll());
+            return Ok(products);
         }
     }
 }
