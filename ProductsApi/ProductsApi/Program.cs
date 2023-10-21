d4adb02b2e0e1167b4796ec940bdcfb418b4ea69
Elvin Mammadov <elvin.mamedov.2013@gmail.com>
2023-10-21 23:51:38 +0400

    Change Program.cs from minimal api to create HostBuilder with Startup configurations

--- ProductsApi/ProductsApi/Program.cs
+++ ProductsApi/ProductsApi/Program.cs
@@ -1,25 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using ProductsApi;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Programm
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+
+
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBulder =>
+                webBulder.UseStartup<Startup>());
+
+
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
