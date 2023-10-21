92f3ee3566a3dac23a6fad9d1da7753b2229f40b
Elvin Mammadov <elvin.mamedov.2013@gmail.com>
2023-10-22 01:30:20 +0400

    Add Products

---
+++ ProductsApi/ProductsApi/Data/Products.cs
@@ -0,0 +1,13 @@
+namespace ProductsApi.Data;
+
+public class Products
+{
+    
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public decimal Price { get; set; } 
+    
+    public string Description { get; set; }
+}
