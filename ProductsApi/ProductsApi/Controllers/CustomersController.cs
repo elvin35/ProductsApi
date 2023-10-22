c23bd49352cbbdb230a57c40f2d798d44d5656cf
Elvin Mammadov <elvin.mamedov.2013@gmail.com>
2023-10-22 07:30:05 +0400

    Add HttpPost(AddCustomer method)

--- ProductsApi/ProductsApi/Controllers/CustomersController.cs
+++ ProductsApi/ProductsApi/Controllers/CustomersController.cs
@@ -1,6 +1,9 @@
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Data;
+using ProductsApi.Models;
 using ProductsApi.Repository;
 
 namespace ProductsApi.Controllers;
@@ -10,10 +13,21 @@ namespace ProductsApi.Controllers;
 public class CustomersController : ControllerBase
 {
     private readonly ICustomersRepository _customersRepository;
-
-    public CustomersController(ICustomersRepository customersRepository)
+    private readonly IMapper _mapper;
+    
+    public CustomersController(ICustomersRepository customersRepository, IMapper mapper)
     {
         _customersRepository = customersRepository;
+        _mapper = mapper;
+
     }
     
+    [HttpPost("AddCustomer")]
+    public async Task<IActionResult> AddCustomer(CustomerModel customer)
+    {
+        var newCustomer = _mapper.Map<Customers>(customer);
+        
+        await _customersRepository.Add(newCustomer);
+        return Ok();
+    }
 }
