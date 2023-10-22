ba9d116cb75d7bbd9268eb70fea77ba899bb4c02
Elvin Mammadov <elvin.mamedov.2013@gmail.com>
2023-10-22 06:22:33 +0400

    Add CustomerModel

---
+++ ProductsApi/ProductsApi/Models/CustomerModel.cs
@@ -0,0 +1,11 @@
+namespace ProductsApi.Models;
+
+public class CustomerModel
+{
+  
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public decimal Balance { get; set; }
+}
