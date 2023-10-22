a23d967e65a8c67e37f0248a9ba28d339a3f799e
Murad <mrdaydinov@gmail.com>
2023-10-22 16:17:25 +0400

    Add CheckNegativeAttribute

---
+++ ProductsApi/ProductsApi/CheckNegativeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsApi
+{
+    public class CheckNegativeAttribute : ValidationAttribute
+    {
+        public CheckNegativeAttribute()
+        {
+
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if ((decimal)value < 0)
+                return false;
+            return true;
+        }
+    }
+}
